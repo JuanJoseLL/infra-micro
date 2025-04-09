f42b81556a7b26dd91e81bed1d2f9bc30290b975
JuanJoseLL <1001367985@u.icesi.edu.co>
2025-04-09 18:59:05 -0500

    resolve redis connectivity issues

--- main.tf
+++ main.tf
@@ -20,14 +20,53 @@ resource "azurerm_virtual_network" "vnet" {
   tags                = local.tags
 }
 
+resource "azurerm_private_dns_zone" "redis_private_zone" {
+  name                = "privatelink.redis.cache.windows.net" 
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "redis_zone_link" {
+  name                  = "${local.resource_prefix}-redis-zone-link"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.redis_private_zone.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled  = false 
+  tags                  = local.tags
+}
+
+resource "azurerm_private_endpoint" "redis_pe" {
+  name                = "${module.redis.name}-pe" 
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = azurerm_subnet.aks_subnet.id 
+  tags                = local.tags
+
+  private_service_connection {
+    name                           = "${module.redis.name}-psc"
+    private_connection_resource_id = module.redis.id 
+    is_manual_connection           = false
+    subresource_names              = ["redisCache"] 
+  }
+
+  private_dns_zone_group {
+    name                 = "redis-dns-group" 
+    private_dns_zone_ids = [azurerm_private_dns_zone.redis_private_zone.id]
+  }
+
+  #
+  depends_on = [
+    module.redis 
+  ]
+}
 resource "azurerm_subnet" "aks_subnet" {
-  name                 = "aks-subnet" # Standard name
+  name                 = "aks-subnet" 
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-  service_endpoints    = ["Microsoft.ContainerRegistry", "Microsoft.Cache"]
-
+  service_endpoints    = ["Microsoft.ContainerRegistry"]
 }
+
 
 resource "azurerm_network_security_group" "aks_nsg" {
   name                = "${local.resource_prefix}-aks-nsg"
